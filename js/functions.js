c8a9e249d05c9dacd478f2d291d50495b9c291de
Lucas Policarpo <empresariallrp@gmail.com>
2023-02-22 00:15:47 -0300

    feat: add new module (NUM-01)

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,44 @@
 window.onload = function(){
-    alert("Olá Mundo");
+    
+    var map; 
+    function initialize(){
+        var mapProp = {
+            center: new google.maps.LatLng(-24.1835,-46.7895),
+            scrollWheel: false,
+            zoom: 12,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        map = new google.maps.Map(document.getElementById("mapa"),mapProp);
+    }
+    function addMarker(lat,long,icon,content,click){
+        var latLng = {'lat':lat,'lng':long};
+        var marker = new google.maps.Marker({
+            position:latLng,
+            map: map,
+            icon: icon,
+
+        });
+
+        var infoWindow = new google.maps.infoWindow({
+            content: content,
+            maxWidth: 200,
+            pixelOffset: new google.maps.Size(0,20)
+        });
+
+        if(click == true){
+        google.maps.event.addListener(marker,'click',function(){
+            infoWindow.open(map,marker);
+        });
+    }
+        else{
+        infoWindow.open(map,marker);
+    }
+        
+    }
+
+    initialize();
+    var conteudo = '<p style="color:black;font-size:13px;padding:13px 0;">Meu Endereço!</p>';
+    addMarker(-24.182777,-46.788139,'','Meu endereço personalizado!',true);
+
 }
